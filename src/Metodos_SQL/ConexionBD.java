dc86b69cdb687802c57f92c1ac2f4648a16b93d3
Enrique Ortiz <enriquotero@gmail.com>
2022-01-04 15:16:18 -0500

    Se agrego la conexion a la base de datos

--- src/Metodos_SQL/ConexionBD.java
+++ src/Metodos_SQL/ConexionBD.java
@@ -5,10 +5,30 @@
  */
 package Metodos_SQL;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author copar
  */
 public class ConexionBD {
-    
+  public static String url = "jdbc:mysql://localhost/login_bd";
+    public static String usuario = "Marlon";
+    public static String contraseña = "marlon123";
+    public static String clase = "com.mysql.jdbc.Driver";
+
+    public static Connection conectar() {
+        Connection conexion = null;
+
+        try {
+            Class.forName(clase);
+            conexion = (Connection) DriverManager.getConnection(url, usuario, contraseña);
+            System.out.println("Conexion establecida");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e);
+        }
+        return conexion;
+    }  
 }
